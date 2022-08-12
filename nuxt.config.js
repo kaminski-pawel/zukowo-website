493e43a14604524f9866e38a7552c3381399e027
Pawel Kaminski <pawelkaminski@tipi.software>
2022-08-12 14:06:20 +0200

    style: config responsive viewport

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,10 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
